efda13f58a0902c4970c464f8ca98c9343377cc6
Markus Ecker <markus.ecker@gmail.com>
2025-03-31 14:37:25 +0200

    update mastra agent

--- src/mastra-agent.ts
+++ src/mastra-agent.ts
@@ -1,8 +1,13 @@
 import { HttpAgent } from "@agentwire/client";
 
 export class MastraAgent extends HttpAgent {
-  // /** Modify this to customize the request */
-  // protected requestInit(): RequestInit {
+  /**
+   * Returns the fetch config for the http request.
+   * Override this to customize the request.
+   *
+   * @returns The fetch config for the http request.
+   */
+  // protected requestInit(input: AgentInput): RequestInit {
   //   return {
   //     method: "POST",
   //     headers: {
@@ -10,7 +15,7 @@ export class MastraAgent extends HttpAgent {
   //       "Content-Type": "application/json",
   //       Accept: "text/event-stream",
   //     },
-  //     body: JSON.stringify(this.input()),
+  //     body: JSON.stringify(input),
   //     signal: this.abortController.signal,
   //   };
   // }
